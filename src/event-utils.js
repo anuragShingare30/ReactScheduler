a27f56f181fe517fb5c0411f39e0082ff358c29a
Anurag Pramod Shingare <anuragshingare2005@gmail.com>
2024-08-05 21:20:31 +0530

    First Commit

---
+++ src/event-utils.js
@@ -0,0 +1,20 @@
+let eventGuid = 0
+let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+
+export const INITIAL_EVENTS = [
+  {
+    id: createEventId(),
+    title: 'Event 2', 
+    start: todayStr
+  },
+  {
+    id: createEventId(),
+    title: 'Event 1',
+    start: todayStr
+  }
+]
+
+
+export function createEventId() {
+  return String(eventGuid++)
+}
